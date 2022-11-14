380084dcbaffb06fe4074519188ccaea857b6ae4
meghanathpro <meghanath.1994v@gmail.com>
2022-11-14 10:47:23 +0530

    firbase db connected

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -6,6 +6,8 @@ import {
   GoogleAuthProvider,
 } from "firebase/auth";
 
+import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
+
 const firebaseConfig = {
   apiKey: "AIzaSyBnyZav-8s2c_kbqcb7WyLYWzIpmL6BKQo",
   authDomain: "drop-clothing-db.firebaseapp.com",
@@ -25,3 +27,33 @@ provider.setCustomParameters({
 
 export const auth = getAuth();
 export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+export const db = getFirestore();
+export const createUseDocFromAuth = async (userAuth) => {
+  const userDocRef = doc(db, "users", userAuth.uid);
+  console.log(userDocRef);
+
+  const userSnapshot = await getDoc(userDocRef);
+  console.log(userSnapshot);
+  console.log(userSnapshot.exists());
+
+  //if user data doesnot exist
+  // create set the document with the data from userAuth in mycollection
+
+  if (!userSnapshot.exists()) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await setDoc(userDocRef, { displayName, email, createdAt });
+    } catch (error) {
+      console.log("err creating user", error.message);
+    }
+  }
+
+  return userDocRef;
+
+  //if user data exists
+
+  //return userdocref
+};

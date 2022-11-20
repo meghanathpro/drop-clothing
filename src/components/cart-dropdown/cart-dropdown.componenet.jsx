22f8b7d7ed02b3375f0a98fd5ca71c5a35aa53bf
meghanathpro <meghanath.1994v@gmail.com>
2022-11-20 19:45:12 +0530

    cart updates

--- src/components/cart-dropdown/cart-dropdown.componenet.jsx
+++ src/components/cart-dropdown/cart-dropdown.componenet.jsx
@@ -1,12 +1,20 @@
-import "./cart-dropdown.styles.scss";
+import { useContext } from "react";
+import { CartContext } from "../../contexts/cart.context";
 import Button from "../button/button.component";
 
+import CartItem from "../cart-item/cart-item.componenets";
+import "./cart-dropdown.styles.scss";
+
 const CartDropDown = () => {
+  const { cartItems } = useContext(CartContext);
   return (
     <div className="cart-dropdown-container">
       <div className="cart-items">
-        <Button> GO TO CHECKOUT </Button>
+        {cartItems.map((item) => (
+          <CartItem key={item.id} cartItem={item} />
+        ))}
       </div>
+      <Button> GO TO CHECKOUT </Button>
     </div>
   );
 };
